e1a645673414c8af237cbcd2517497cb28f64b4e
johnsondeborahz96390 <johnson_deborahz96390@gmx.com>
2025-03-08 05:19:33 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int a = rand() % 10 + 1;
+    int b = rand() % 10 + 1;
+    int c = rand() % 10 + 1;
+    int d = rand() % 10 + 1;
+    int e = rand() % 10 + 1;
+    int f = rand() % 10 + 1;
+    int g = rand() % 10 + 1;
+    int h = rand() % 10 + 1;
+    int i = rand() % 10 + 1;
+    int j = rand() % 10 + 1;
+    int k = rand() % 10 + 1;
+    int l = rand() % 10 + 1;
+    int m = rand() % 10 + 1;
+    int n = rand() % 10 + 1;
+    int o = rand() % 10 + 1;
+    int p = rand() % 10 + 1;
+    int q = rand() % 10 + 1;
+    int r = rand() % 10 + 1;
+    int s = rand() % 10 + 1;
+    int t = rand() % 10 + 1;
+    int u = rand() % 10 + 1;
+    int v = rand() % 10 + 1;
+    int w = rand() % 10 + 1;
+    int x = rand() % 10 + 1;
+    int y = rand() % 10 + 1;
+    int z = rand() % 10 + 1;
+
+    printf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z);
+
+    return 0;
+}
